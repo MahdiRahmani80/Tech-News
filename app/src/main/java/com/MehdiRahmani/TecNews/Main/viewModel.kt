67f53fdd531e236ca94921a59b9dfeb558bd31e1
Mahdi Rahmani <mahdirahmani8@yahoo.com>
2022-08-10 22:02:02 +0430

    better solotion to show splash fragment

--- app/src/main/java/com/MehdiRahmani/TecNews/Main/viewModel.kt
+++ app/src/main/java/com/MehdiRahmani/TecNews/Main/viewModel.kt
@@ -1,13 +1,10 @@
 package com.MehdiRahmani.TecNews.Main
 
-import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.MehdiRahmani.TecNews.Home.HomeFragment
 import com.MehdiRahmani.TecNews.Model.Articles
-import com.MehdiRahmani.TecNews.Model.News
 import com.MehdiRahmani.TecNews.SingleNewsPage.SingleNewsFragment
 import java.util.*
 import kotlin.concurrent.schedule
@@ -39,10 +36,11 @@ class MainViewModel : ViewModel() {
     private fun loadFragment() {
 
 
-        Timer().schedule(1) {
+        Thread{
             fragment_state = SplashFragment()
             fr.postValue(fragment_state)
-        }
+        }.start()
+
         Timer().schedule(2000) {
             fragment_state = HomeFragment()
             fr.postValue(fragment_state)
